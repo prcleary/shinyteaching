75250ebf5aa03ef366b86e9ea1f30535f90a1281
Paul Cleary <paul.cleary@ukhsa.gov.uk>
2024-10-06 12:50:11 +0100

    Add code to run from GitHub

--- app.R
+++ app.R
@@ -66,7 +66,7 @@ front_page_ui <- fluidPage(
 
   To run this app on your own computer, run the following code in RStudio:
   ```r
-  install.packages(c('shiny', 'htmltools', 'markdown', 'DT', 'data.table', 'bit64', 'readxl')
+  install.packages(c('shiny', 'htmltools', 'markdown', 'DT', 'data.table', 'bit64', 'readxl'))
   shiny::runGitHub('prcleary/shinyteaching')
   ````
   ### Learning the basics
